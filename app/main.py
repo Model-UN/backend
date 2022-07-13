19d0a76c648f8016b11acc2bac5627daee4c62b6
James He <james.he@modelun.net>
2022-07-12 19:13:40 -0600

    ROBOT-4: CORS for *

--- app/main.py
+++ app/main.py
@@ -13,9 +13,10 @@ origins = [
     "http://localhost",
     "http://localhost:3000"
 ]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
